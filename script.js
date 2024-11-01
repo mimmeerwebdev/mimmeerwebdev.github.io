f3ae9d00172d752a1912d6bc5c45265cf66f7389
mimwebdev <hubg3445@gmail.com>
2024-11-01 11:31:14 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const urlParams = new URLSearchParams(window.location.search);
+const tutorial = urlParams.get('tutorial');
+const page = urlParams.get('page');
+const mainDocElement = document.getElementById('maindoc');
+
+// Construct the file path based on tutorial and page parameters
+const filePath = `tutorials/${tutorial}/page${page}.txt`;
+const navPath = `core/nav.html`;
+const footPath = `core/foot.html`;
+
+// Fetch the content from the specified file
+fetch(filePath)
+  .then(response => response.text())
+  .then(text => {
+    // Split the text into lines
+    const lines = text.split('\n');
+
+    // Update the title within the existing `<title>` tag
+    document.querySelector('title').textContent = lines[0];
+
+    // Display the remaining lines as content within the target element
+    mainDocElement.innerHTML = lines.slice(1).join('<br>');
+  })
+  .catch(error => {
+    // Display a custom 404 message
+    mainDocElement.innerHTML = `<h1>404</h1><p>This tutorial is missing! Where is it? To try and find it check that URL at the top!</p>`;
+  });
+
+fetch(navPath)
+  .then(response => response.text())
+  .then(navHTML => {
+    const navElement = document.querySelector('header');
+    navElement.innerHTML = navHTML;
+  })
+  .catch(error => {
+    const navElement = document.querySelector('header');
+    console.error('Sorry, navigation is not avalible. Error:', error);
+    navElement.innerHTML = "Sorry, the header isn&apos;t here.";
+  });
+
+fetch(footPath)
+  .then(response => response.text())
+  .then(footHTML => {
+      const footerElement = document.querySelector('footer');
+    document.querySelector('footer').innerHTML = footHTML;
+      footerElement.innerHTML = footHTML;
+  })
+  .catch(error => {
+      const footerElement = document.querySelector('footer');
+    console.error('Sorry, the footer isn&apost here:', error);
+    footerElement.innerHTML = "Sorry, the footer isn&apos;t here.";
+  });
